83dc78b4f57d1ed2d2b93c0fa374274d4d164c6a
unknown <victoriadlai@gmail.com>
2018-05-17 23:49:26 -0700

    reformatted dashboard

--- client/src/components/DashboardLink.jsx
+++ client/src/components/DashboardLink.jsx
@@ -5,16 +5,23 @@ import './DashboardLink.css';
 class DashboardLink extends Component {
     render() {
         return (
-            <div className="dashboard-item">
+            <button className="dashboard-item" to={this.props.to}>
+                
                 <div className="dashboard-item-icon">
+                    <img src={this.props.icon}/>
                 </div>
-                <div className="dashboard-item-description">
-                    <Link to={this.props.to}>
-                        <h3>{this.props.text}</h3>
-                        <p>{this.props.description}</p>
-                    </Link>
+
+                <div className="dashboard-item-text">
+                    <div className="dashboard-item-label">
+                        {this.props.text}
+                    </div>
+                    
+                    <div className="dashboard-item-description">
+                        {this.props.description}
+                    </div>
+
                 </div>
-            </div>
+            </button>
         );
     }
 }
